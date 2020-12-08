4a3a13b7291240dd22f27d8b3e44ec5689f4a346
John Beck <cygnus_33065@yahoo.com>
2020-12-08 14:43:27 -0500

    Working with Alex

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const fetch = require('node-fetch');
 
 fetch('https://swapi.dev/api/people/1')
     .then(response => response.json())
-    .then(person => person.vehicles)
-    .then(vehicle => vehicle[0])
-    .then(apiCall => fetch(apiCall))
-    .then(response => response.json())
-    .then(printVehicle => console.log(printVehicle));
+    .then(person => person.films)
+    .then(filmsArray => Promise.all(filmsArray).then((film) => {
+      film.forEach(film => fetch(film).then(film => film.json()).then(film => console.log(film)))
+    }))
+
+    // .then(person => person.vehicles)
+    // .then(vehicle => vehicle[0])
+    // .then(apiCall => fetch(apiCall))
+    // .then(response => response.json())
+    // .then(printVehicle => console.log(printVehicle));
+
